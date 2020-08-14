a066e7d49bbac9a64c4c6365d907650370331bab
Adnan Hakim <adnanhakim007@gmail.com>
2020-08-14 15:39:24 +0530

    Added improvements

--- app/src/main/java/com/teamvoid/djevents/Activities/HomeActivity.java
+++ app/src/main/java/com/teamvoid/djevents/Activities/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.teamvoid.djevents;
+package com.teamvoid.djevents.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.teamvoid.djevents.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
