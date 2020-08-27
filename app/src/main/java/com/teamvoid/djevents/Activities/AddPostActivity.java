9e27df81eead5c860b5d0f7db0678eb9b272746b
Adnan Hakim <adnanhakim007@gmail.com>
2020-08-27 18:05:21 +0530

    Post Image

--- app/src/main/java/com/teamvoid/djevents/Activities/AddPostActivity.java
+++ app/src/main/java/com/teamvoid/djevents/Activities/AddPostActivity.java
@@ -1,17 +1,34 @@
 package com.teamvoid.djevents.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.teamvoid.djevents.R;
 import com.teamvoid.djevents.Utils.Constants;
+
+import java.io.File;
+import java.util.Objects;
 
 public class AddPostActivity extends AppCompatActivity {
 
@@ -20,6 +37,16 @@ public class AddPostActivity extends AppCompatActivity {
     // Elements
     private ImageButton ibBack;
     private ImageView ivImage;
+    private Button btnPost;
+    private ProgressBar progressBar;
+
+    // Variables
+    private Uri photoUri;
+    private String photoPath;
+
+    // Firebase
+    private FirebaseFirestore db;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +58,8 @@ public class AddPostActivity extends AppCompatActivity {
 
         Intent callingIntent = getIntent();
         if (callingIntent.hasExtra(Constants.PHOTO_PATH)) {
-            String photoPath = callingIntent.getStringExtra(Constants.PHOTO_PATH);
-            Uri photoUri = Uri.parse(photoPath);
+            photoPath = callingIntent.getStringExtra(Constants.PHOTO_PATH);
+            photoUri = Uri.fromFile(new File(photoPath));
             Log.d(TAG, "onCreate: Uri: " + photoUri);
 
             ivImage.setImageURI(photoUri);
@@ -40,6 +67,10 @@ public class AddPostActivity extends AppCompatActivity {
 
         // Clicks
         ibBack.setOnClickListener(view -> this.onBackPressed());
+
+        btnPost.setOnClickListener(view -> {
+            uploadImage();
+        });
     }
 
     private void init() {
@@ -47,6 +78,46 @@ public class AddPostActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ibBack = findViewById(R.id.ibAddPostBack);
         ivImage = findViewById(R.id.ivAddPostImage);
+        btnPost = findViewById(R.id.btnAddPost);
+        progressBar = findViewById(R.id.progressAddPost);
+
+        db = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://dj-events-9d44b.appspot.com/posts");
+    }
+
+    private void uploadImage() {
+        if (photoUri != null) {
+            String extension = photoPath.substring(photoPath.lastIndexOf("."));
+            String fileName = System.currentTimeMillis() + extension;
+            Log.d(TAG, "uploadFile: File Name: " + fileName);
+            StorageReference fileReference = storageReference.child(fileName);
+            fileReference.putFile(photoUri)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: Photo upload successful");
+                            Toast.makeText(AddPostActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                            fileReference.getDownloadUrl()
+                                    .addOnCompleteListener(task1 -> {
+                                        if (task1.isSuccessful() && task1.getResult() != null) {
+                                            String downloadUrl = task1.getResult().toString();
+                                            savePost(downloadUrl);
+                                        } else {
+                                            Log.d(TAG, "onComplete: Photo upload failed: " + Objects.requireNonNull(task1.getException()).getStackTrace().toString());
+                                            Toast.makeText(AddPostActivity.this, "Photo upload failed", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        } else {
+                            Log.d(TAG, "onComplete: Photo upload failed: " + Objects.requireNonNull(task.getException()).getStackTrace().toString());
+                            Toast.makeText(AddPostActivity.this, "Photo upload failed", Toast.LENGTH_SHORT).show();
+                            Exception e = task.getException();
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+
+    private void savePost(String imageUrl) {
+
     }
 
     @Override
--- app/src/main/java/com/teamvoid/djevents/Utils/Constants.java
+++ app/src/main/java/com/teamvoid/djevents/Utils/Constants.java
@@ -22,6 +22,7 @@ public class Constants {
     public static final String POSTS = "posts";
     public static final String FOLLOWERS = "followers";
     public static final String IMAGE_URL = "imageUrl";
+    public static final String DP = "dp";
 
     public static final String PHOTO_PATH = "photo_path";
 }
